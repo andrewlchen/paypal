f871c5cb158937556ea985cd47ff392dbae7f3c1
Andrew Chen <sirchen@gmail.com>
2013-07-04 12:55:13 -0700

    paypal package code changes - 4

--- standard/ipn/signals.py
+++ standard/ipn/signals.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Note that sometimes you will get duplicate signals emitted, depending on configuration of your systems. 
 If you do encounter this, you will need to add the "dispatch_uid" to your connect handlers:
 http://code.djangoproject.com/wiki/Signals#Helppost_saveseemstobeemittedtwiceforeachsave
@@ -6,28 +6,30 @@ http://code.djangoproject.com/wiki/Signals#Helppost_saveseemstobeemittedtwicefor
 """
 from django.dispatch import Signal
 
-paypal_ipn_signal = Signal()
+# paypal_ipn_signal = Signal()
 
-# # Sent when a payment is successfully processed.
-# payment_was_successful = Signal()
+# Sent when a payment is successfully processed.
+payment_was_successful = Signal()
 
-# # Sent when a payment is flagged.
-# payment_was_flagged = Signal()
+# Sent when a payment is flagged.
+payment_was_flagged = Signal()
 
-# # Sent when a subscription was cancelled.
-# subscription_cancel = Signal()
+# Sent when a subscription was cancelled.
+subscription_cancel = Signal()
 
-# # Sent when a subscription expires.
-# subscription_eot = Signal()
+# Sent when a subscription expires.
+subscription_eot = Signal()
 
-# # Sent when a subscription was modified.
-# subscription_modify = Signal()
+# Sent when a subscription was modified.
+subscription_modify = Signal()
 
-# # Sent when a subscription is created.
-# subscription_signup = Signal()
+# Sent when a subscription is created.
+subscription_signup = Signal()
 
-# recurring_create = Signal()
-# recurring_payment = Signal()
-# recurring_cancel = Signal()
-# recurring_suspend = Signal()
-# recurring_reactivate = Signal()
+recurring_create = Signal()
+recurring_payment = Signal()
+recurring_cancel = Signal()
+recurring_suspend = Signal()
+recurring_reactivate = Signal()
+recurring_skipped = Signal()
+recurring_failed = Signal()
