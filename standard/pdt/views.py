69324bf18a02ff0a6d74034aa12a0505b09f0d7d
Andrew Chen <sirchen@gmail.com>
2013-07-08 01:23:55 -0700

    paypal package code changes - 6

--- standard/pdt/views.py
+++ standard/pdt/views.py
@@ -50,7 +50,7 @@ def pdt(request, item_check_callable=None, template="pdt/pdt.html", context=None
  
     request.session['pdt_obj'] = pdt_obj
 
-    return HttpResponseRedirect('/oauth_login/authenticate_user/')
+    return HttpResponseRedirect('/home/group/')
 
     # context.update({"failed":failed, "pdt_obj":pdt_obj})
     # return render_to_response(template, context, RequestContext(request))
